40c57330edb8d00fe0ef25fa55f26cfbe07fd636
Manuelde la torre <[mtorrecano@gmail.com]>
2017-11-06 13:53:58 +0100

    nuevo archivo: estructurasDatos/teoria/OperacionesLogicas.java

---
+++ estructurasDatos/teoria/OperacionesLogicas.java
@@ -0,0 +1,10 @@
+public class OperacionesLogicas{
+		public static void main(String[] args){
+			boolean variable1 = 5 > 4 * 3;
+			System.out.println("Debe dar false: " +variable1 );
+			variable1 = 7 * 3 >=5 /1;			
+		}
+	
+	
+	
+}
