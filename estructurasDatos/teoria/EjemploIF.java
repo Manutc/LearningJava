3c0ab67353efc4d5799915e39234d824fc32cbb2
Manuelde la torre <[mtorrecano@gmail.com]>
2017-11-06 14:44:16 +0100

    nuevo archivo: estructurasDatos/teoria/EjemploIF.java

---
+++ estructurasDatos/teoria/EjemploIF.java
@@ -0,0 +1,13 @@
+public class EjemploIF{
+		public static void main(String[] args){
+			int numero = 77;
+			
+			if ( numero % 7 == 0){
+				System.out.println( numero+ " es divisible por 7");
+			}
+			if ( numero % 11 == 0){
+				System.out.println( numero+ " es divisible por 11");
+			}
+			System.out.println( "Fin del programa");
+	}
+}
--- estructurasDatos/teoria/OperacionesLogicas.java
+++ estructurasDatos/teoria/OperacionesLogicas.java
@@ -2,9 +2,21 @@ public class OperacionesLogicas{
 		public static void main(String[] args){
 			boolean variable1 = 5 > 4 * 3;
 			System.out.println("Debe dar false: " +variable1 );
-			variable1 = 7 * 3 >=5 /1;			
-		}
-	
-	
-	
+			
+			variable1 = 7 * 3 >=5 /1;	
+			System.out.println("Debe dar true: " +variable1 );
+			
+			variable1 = variable1 && !(5 > 3 * 7);
+			System.out.println("Debe dar true: " +variable1 );
+			
+			variable1 = variable1 & !(5 > 3 * 7);
+			System.out.println("Debe dar true: " +variable1 );
+			
+			variable1 = variable1 || 5 > 3 * 7;
+			System.out.println("Debe dar true: " +variable1 );
+			
+			variable1 = variable1 | 5 > 3 * 7;
+			System.out.println("Debe dar true: " +variable1 );
+			
+		}	
 }
